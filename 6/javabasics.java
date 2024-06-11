5c189e0a0fe9162af7200f538f4c2ddf41ab8ee0
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2024-06-11 20:22:03 +0530

    Initial commit

---
+++ 6/javabasics.java
@@ -0,0 +1,10 @@
+public class javabasics{
+    public static void main(String args[]){
+        int a = 10;
+        int b=5;
+        String name="Jatin Maurya";
+        
+        System.out.println(a);
+        System.out.println("My name is " + name);
+    }
+}
