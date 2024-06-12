4c5617b2513510867b0df4a5657a187c048ab141
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2024-06-12 09:49:47 +0530

    Program of addition

---
+++ 6/dyn_sum_of_2_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class dyn_sum_of_2_number {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int fvalue = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int svalue = sc.nextInt();
+        int sum = fvalue + svalue;
+        System.out.println("Addition = " + sum);
+
+    }
+}
---
+++ 6/sum_of_2_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sum_of_2_number {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+
+        int a = 2;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
