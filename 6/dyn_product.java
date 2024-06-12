4a71f9505e79984ddfe5e0486b546f6b66686470
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2024-06-12 11:00:22 +0530

    basics maths

---
+++ 6/dyn_product.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class dyn_product {
+  public static void main(String args[]){
+    // This Program will calculate the Product of Two number
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter First value");
+     int fvalue = sc.nextInt();
+     System.out.println("Enter second value");
+     int svalue = sc.nextInt();
+     int product = fvalue * svalue;
+     System.out.println("Product = " + product);
+  }  
+}
