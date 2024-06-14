20a6a5eeee661c3ab1efe325697922e83e4497f0
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2024-06-15 04:25:04 +0530

    function

---
+++ 11. Function and methods/sum_of_a_and_b.java
@@ -0,0 +1,13 @@
+public class sum_of_a_and_b {
+
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        sum(5, 6);
+        sum(9, 9);
+    }
+}
