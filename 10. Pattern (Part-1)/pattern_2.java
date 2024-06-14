6e89c6d89bea1c9aea4f8f64a30182800fc825d6
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2024-06-14 20:01:13 +0530

    pattern

---
+++ 10. Pattern (Part-1)/pattern_2.java
@@ -0,0 +1,10 @@
+public class pattern_2 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; i <= j; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
