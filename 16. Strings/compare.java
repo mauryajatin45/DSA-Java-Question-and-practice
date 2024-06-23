b8fad5f0d7f63f4796fae158de47bc8dbd602c93
Jatin Maurya <mauryajatin45@gmail.com>
2024-06-23 13:22:54 +0530

    compareTO in Strings

---
+++ 16. Strings/compare.java
@@ -0,0 +1,20 @@
+public class compare {
+
+    public static void comparision(String fruits[]){
+
+            String largest = fruits[0];
+            for(int i=1; i<fruits.length; i++){
+                if(largest.compareTo(fruits[i])<0){
+                    largest = fruits[i];
+                }
+            }
+            System.out.println(largest);
+
+
+    }
+
+    public static void main(String[] args) {
+        String fruits[] = {"Apple", "Banana", "mango", "chickoo" };   
+        comparision(fruits);
+    }
+}
