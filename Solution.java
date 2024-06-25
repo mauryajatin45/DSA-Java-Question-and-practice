0e7c1b89dcb533a80ad9ca291ade7c1f94c7bf3c
Jatin Maurya <mauryajatin45@gmail.com>
2024-06-25 08:46:41 +0530

    commit

--- Solution.java
+++ Solution.java
@@ -1,13 +1,22 @@
-public class Solution {
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-    public static void check() {
-        int x = 123;
-        while(x>0){
-            
+public class Solution {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int arr[] = mergeArraysUsingStreams(nums1, nums2);
+        Arrays.sort(arr);
+        int add = 0;
+        for (int i; i < arr.length; i++) {
+            add += arr[i];
         }
+        double op;
+        return op = add / arr.length;
+
     }
 
     public static void main(String[] args) {
-        check();
+        int nums1[] = { 1, 3 };
+        int nums2[] = { 2, 4 };
+        System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 }
