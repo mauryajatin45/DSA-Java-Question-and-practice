3bde3e120e576cc9a5b60f7fec3f1c53c145aaff
Jatin Maurya <mauryajatin45@gmail.com>
2024-06-26 12:30:51 +0530

    multiple bit programs

---
+++ 17. Bit Manipulation/ithbit.java
@@ -0,0 +1,25 @@
+public class ithbit {
+
+    public static int getithvalue(int n, int i) {
+        int bitmask = 1 << i;
+        if ((n & bitmask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static int setithvalue(int n, int i) {
+        int bitmask = 1 << i;
+        return n | bitmask;
+    }
+
+    public static int clearithvalue(int n, int i) {
+        int bitmask = ~(1 << i);
+        return n & bitmask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearithvalue(10, 1));
+    }
+}
