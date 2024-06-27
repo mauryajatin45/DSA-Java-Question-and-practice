21b2afd162ac9072c8232734c98a5eaa73363658
Jatin Maurya <mauryajatin45@gmail.com>
2024-06-27 19:17:59 +0530

    commit

--- Solution.java
+++ Solution.java
@@ -1,16 +1,9 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.*;
-
-public class Solution {
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int nums = nums1[true] + nums2[true];
-        return nums;
+class Solution {
+    public static int mySqrt(int x) {
+        int result =(int) Math.sqrt(x);
+       return result;
     }
-
     public static void main(String[] args) {
-        int nums1 = {1, 3};
-        int nums2 = {2};
-        System.out.println(findMedianSortedArrays(nums1, nums2));
+System.out.println(mySqrt(4));
     }
 }
