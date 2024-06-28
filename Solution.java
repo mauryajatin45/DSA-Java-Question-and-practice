f7d71fd926da732211b7651011e3aed8e35701a8
Jatin Maurya <mauryajatin45@gmail.com>
2024-06-28 09:21:33 +0530

    commit

--- Solution.java
+++ Solution.java
@@ -1,9 +1,13 @@
 class Solution {
-    public static int mySqrt(int x) {
-        int result =(int) Math.sqrt(x);
-       return result;
+    public static int threeSumClosest(int[] nums, int target) {
+        int add = 0;
+        for(int i = 0; i<=nums.length; i++){
+            add = add + 
+        }
     }
     public static void main(String[] args) {
-System.out.println(mySqrt(4));
+        int nums[] = {-1,2,1,-4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
     }
 }
