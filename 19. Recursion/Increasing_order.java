82fe1eafe512b18f5a5ee3a939f09efbb66f5160
Jatin Maurya <mauryajatin45@gmail.com>
2024-07-02 20:02:46 +0530

    Commit

---
+++ 19. Recursion/Increasing_order.java
@@ -0,0 +1,14 @@
+public class Increasing_order {
+    public static void inc(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        inc(n - 1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        inc(10);
+    }
+}
